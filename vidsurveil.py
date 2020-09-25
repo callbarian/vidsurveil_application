7a901a3a58267f9a553835cf16437bc21ede4862
이성민 <21100492@handong.edu>
2020-09-25 16:37:16 +0900

    seongmin

---
+++ vidsurveil.py
@@ -0,0 +1,32 @@
+# This Python file uses the following encoding: utf-8
+import sys
+import os
+'''
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PySide2.QtCore import (QCoreApplication, QMetaObject, QObject, QPoint,
+    QRect, QSize, QUrl, Qt)
+from PySide2.QtGui import (QBrush, QColor, QConicalGradient, QCursor, QFont,
+    QFontDatabase, QIcon, QLinearGradient, QPalette, QPainter, QPixmap,
+    QRadialGradient)
+from PySide2.QtWidgets import *
+from PySide2.QtWidgets import QApplication
+'''
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+from mainwindow import Ui_MainWindow
+
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        QtWidgets.QMainWindow.__init__(self)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.ui.draw_miniview(os.getcwd()+'/test.mp4')
+        self.show()
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+
+    window = MainWindow()
+    # window.show()
+    sys.exit(app.exec_())
