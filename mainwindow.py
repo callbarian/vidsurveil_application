0e978afdb26a5b105c5f3e6714e7f6398e56a5e1
이성민 <21100492@handong.edu>
2020-09-28 19:41:45 +0900

    seongmin

--- mainwindow.py
+++ mainwindow.py
@@ -214,38 +214,7 @@ class Ui_MainWindow(QtWidgets.QMainWindow):
         self.actionAdd_Files.setText(_translate("MainWindow", "Add Files"))
         self.actionAbout.setText(_translate("MainWindow", "About"))
    
-    def draw_miniview(self,dir):
-        scene = QGraphicsScene(self)
-        image = self.show_miniview(dir)
-        #self.imgQ = ImageQt.ImageQt(image)
-        #print(image.shape[1],image.shape[0])
-        img = QImage(image.data,image.shape[1],image.shape[0],QImage.Format_RGB888).rgbSwapped()
-        pixMap =  QPixmap.fromImage(img)
-        item = QGraphicsPixmapItem(pixMap)
-        scene.addItem(item)
-        self.mini_view_1.setScene(scene)
-        self.mini_view_2.setScene(scene)
     
-    def show_miniview(self,dir):
-        f = cv2.VideoCapture(dir)
-        total_frames = f.get(cv2.CAP_PROP_FRAME_COUNT)
-        frame_rate = f.get(cv2.CAP_PROP_FPS)
-        total_length = int(total_frames/frame_rate)
-        print('total frames:{}, frame_rate :{} '.format(total_frames,frame_rate))
-        print('total length:{} '.format(total_length))
-        #print(f.get(cv2.CAP_PROP_POS_FRAMES))
-        # the size will be 80*80*11 (width 80, length 80, 11 images)
-        interval = int(total_frames/11)
-        frame_list = []
-        for i in range(0,11):
-            f_num = i*interval
-            f.set(cv2.CAP_PROP_POS_FRAMES,f_num)
-            ret,frame = f.read()
-            assert ret
-            frame_list.append(cv2.resize(frame,(80,80)))
-        frame_concat = cv2.hconcat(frame_list)
-        return frame_concat
-
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
