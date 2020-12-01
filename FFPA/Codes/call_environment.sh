91a8c83b0f75957aad15a6c6bf5c88eaa255ad20
이성민 <21100492@handong.edu>
2020-12-01 17:47:36 +0900

    s

--- FFPA/Codes/call_environment.sh
+++ FFPA/Codes/call_environment.sh
@@ -2,5 +2,5 @@
 
 echo "Executing a bash statement"
 export LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib64
-$1 $2 $3 $4 $5 --env_path=$1 --script_path=$2 --dataset=$3 --test_folder=$4 --gpu=$5
+$1 $2  --env_path=$1 --script_path=$2 
 
